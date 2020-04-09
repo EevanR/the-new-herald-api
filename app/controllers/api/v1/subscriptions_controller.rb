bafb49e7989e5d8c169e44285d1791f9a27df3e4
Eevan <eevan.redon@gmail.com>
2020-04-09 20:31:53 +0200

    remove stripe

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,36 +1,36 @@
 class Api::V1::SubscriptionsController < ApplicationController
   before_action :authenticate_current_user
-  rescue_from Stripe::InvalidRequestError, with: :invalid_token_id
+  # rescue_from Stripe::InvalidRequestError, with: :invalid_token_id
 
-  def create
+  # def create
 
-    if params[:stripeToken]
-      customer = Stripe::Customer.create(
-        email: current_user.email,
-        source: params[:stripeToken]
-      )
+  #   if params[:stripeToken]
+  #     customer = Stripe::Customer.create(
+  #       email: current_user.email,
+  #       source: params[:stripeToken]
+  #     )
 
-      charge = Stripe::Charge.create(
-        customer: customer.id,
-        currency: 'sek',
-        amount: 999,
-        description: I18n.t('stripe.description')
-      )
+  #     charge = Stripe::Charge.create(
+  #       customer: customer.id,
+  #       currency: 'sek',
+  #       amount: 999,
+  #       description: I18n.t('stripe.description')
+  #     )
       
-      if (charge.paid)
-        current_user.role = 'subscriber'
-        current_user.save
-        render json: { message: I18n.t('stripe.pay_success')}
-      end
-    else
-      render json: { message: I18n.t('stripe.missing_token')}
-    end
-  end
+  #     if (charge.paid)
+  #       current_user.role = 'subscriber'
+  #       current_user.save
+  #       render json: { message: I18n.t('stripe.pay_success')}
+  #     end
+  #   else
+  #     render json: { message: I18n.t('stripe.missing_token')}
+  #   end
+  # end
   
-  private
+  # private
 
-  def invalid_token_id
-    render json: { message: I18n.t('stripe.pay_failure')}
-  end 
+  # def invalid_token_id
+  #   render json: { message: I18n.t('stripe.pay_failure')}
+  # end 
 
 end
